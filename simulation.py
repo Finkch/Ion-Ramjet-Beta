6b6311cb2c8720773f99ce8539d0cbd9da6eef41
Finkch <skyler@thealdersons.org>
2023-12-31 13:49:50 -0800

    Updated finkchlib submodule

--- simulation.py
+++ simulation.py
@@ -1,11 +1,35 @@
 # This file contains a class that performs the simulation
 # Wrapping it in a class makes for easy referencing
 
+from finkchlib.clock import Clock
+from ramjet import Ramjet
+
+
 class Simulation:
     def __init__(self) -> None:
-        self.exist = True
+        self.exist: bool = True
+
+        # Whether or not to perform debug printouts
+        self.debug = True
+
+        # Seconds per simulation step
+        self.step: int = 1
+
+        # Used to perform printouts
+        self.clock: Clock = Clock(1000 / 60)
+
+        # The craft to simulate
+        self.ramjet: Ramjet = Ramjet("ioRam-Beta", 100, 5, self.step, 10, 1e3, 26, 4.9e4, 1.5e6, 1e3, 1e3, 1e7)
+
 
     # The simulation loop
     def __call__(self):
         while self.exist:
-            pass
+            self.ramjet()
+
+
+            if self.debug and self.clock.time():
+                self.debug()
+
+    def debug(self):
+        print(self.ramjet)
