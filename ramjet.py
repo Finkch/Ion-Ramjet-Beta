5c7dbb66e62d4701676dbcc306c4402b176092a8
Finkch <skyler@thealdersons.org>
2023-12-31 12:47:43 -0800

    Expanded upon thrust, scoop, generate functions

--- ramjet.py
+++ ramjet.py
@@ -1,11 +1,12 @@
 # A spacecraft
 
 from finkchlib.actor import Actor2
+import numpy as np
 
 # A ramjet is our basic spacecraft.
 # This class will act like an interface for our proper crafts
 class Ramjet(Actor2):
-    def __init__(self, name, mass, radius, step, fuel, battery, thrust, v_e, scoop_r, scoop_p, power) -> None:
+    def __init__(self, name, mass, radius, step, fuel, battery, thrust, v_e, engine_power, scoop_r, scoop_p, power) -> None:
         super().__init__(name, mass, radius)
         
         self.step = step
@@ -17,6 +18,8 @@ class Ramjet(Actor2):
         # Thrust parameters
         self.thrust = thrust
         self.v_e = v_e
+        self.m_d = self.thrust / self.v_e
+        self.engine_power = engine_power
 
         # Scoop parameters
         self.scoop_r = scoop_r
@@ -29,14 +32,48 @@ class Ramjet(Actor2):
     def __call__(self) -> None:
         super().__call__(self.step)
 
+
+
+    # Fires the engine, producing thrust
     def fire(self) -> float:
-        pass
+    
+        reaction_mass, mass_throttle = self.tank.pipe_out(self.m_d)
+        reaction_power, power_throttle = self.battery.pipe_out(self.engine_power)
+        
+        # If one source doesn't produce enough, then refund the other source
+        mass_refund = 0
+        power_refund = 0
 
+        self.tank.pipe_in(mass_refund)
+        self.battery.pipe_in(power_refund)
+
+        return reaction_mass * self.v_e 
+
+
+
+    # Scoops up hydrogen from the ISM
     def scoop(self) -> None:
-        pass
+        
+        # Mass of hydrogen swept
+        hydrogen = self.sweep_hydrogen()
 
-    def power(self) -> None:
-        pass
+        # Adds hydrogen to the tank
+        self.tank.pipe_in(hydrogen * self.step)
+
+    # Returns the mass of hydrogen scooped up 
+    def sweep_hydrogen(self):
+        
+        # Gets the ratio of requested power
+        power_throttle = self.battery.pipe_out(self.scoop_p)[1]
+
+        # NOT UP TO DATE
+        # Still needs to account for velocity and area
+        return np.pi * (self.scoop_r * power_throttle) ** 2
+
+
+    # Generates power, adding it to the battery
+    def generate(self) -> None:
+        self.battery.pipe_in(self.power * self.step)
 
 
 
