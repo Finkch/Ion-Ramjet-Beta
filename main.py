67081c521859bc1c817648a4035405c817dc257a
Finkch <skyler@thealdersons.org>
2023-12-31 14:23:50 -0800

    Adjusted setup to allow for DebugSim

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # This file gets the ball rolling
 
-from simulation import Simulation
+from simulation import *
 
 # Gets everything going
 def main():
     
-    simulation = Simulation()
+    debug = True
+
+    # Desired framerate for printouts
+    framerate = 1000 / 60 if not debug else 1000
+
+    simulation = Simulation(framerate) if not debug else DebugSimulation(framerate) 
 
     simulation()
 
