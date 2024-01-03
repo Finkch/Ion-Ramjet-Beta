02eaebbe2148d3c25db5c9f3b7d8d3d21eb07106
Finkch <skyler@thealdersons.org>
2024-01-03 12:13:01 -0800

    Added (incomplete) RelativisticSpacetime

--- spacetime.py
+++ spacetime.py
@@ -1,6 +1,8 @@
 from finkchlib.vector import Vector2
+from finkchlib.constants import c
 
-# Oversees the space and time of a thing
+# Oversees the space and time of a thing.
+# From an observer aboard the craft
 class Spacetime:
     def __init__(self, step, position: Vector2 = Vector2(), velocity: Vector2 = Vector2(), acceleration: Vector2 = Vector2()) -> None:
         self.step: int = step
@@ -29,3 +31,24 @@ class Spacetime:
     # Applies a force to the craft
     def force(self, mass, amount: Vector2) -> None:
         self.acceleration += amount / mass
+
+
+# A spacetime that incorporates special relativity.
+# Same as spacetime, but from an observer on the ground
+class RelativisticSpacetime(Spacetime):
+    def __init__(self, step, position: Vector2 = Vector2(), velocity: Vector2 = Vector2(), acceleration: Vector2 = Vector2()) -> None:
+        super().__init__(step, position, velocity, acceleration)
+
+    def __call__(self) -> None:
+        gamma = self.gamma()
+
+        # Increases time experiences
+        self.time += self.step / gamma
+
+
+    # Returns some useful factors
+    def gamma(self) -> float:
+        return 1 / (1 - self.beta() ** 2)
+    
+    def beta(self) -> float:
+        return self.velocity.hypo() / c
