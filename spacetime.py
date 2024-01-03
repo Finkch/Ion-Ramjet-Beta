fcfab6a47ced50db033576bef22cc58849e8dd31
Finkch <skyler@thealdersons.org>
2024-01-03 12:24:06 -0800

    Removed step as an attribute

--- spacetime.py
+++ spacetime.py
@@ -4,8 +4,7 @@ from finkchlib.constants import c
 # Oversees the space and time of a thing.
 # From an observer aboard the craft
 class Spacetime:
-    def __init__(self, step, position: Vector2 = Vector2(), velocity: Vector2 = Vector2(), acceleration: Vector2 = Vector2()) -> None:
-        self.step: int = step
+    def __init__(self, position: Vector2 = Vector2(), velocity: Vector2 = Vector2(), acceleration: Vector2 = Vector2()) -> None:
         self.time = 0
 
         self.position: Vector2 = position
@@ -15,14 +14,14 @@ class Spacetime:
         self.acceleration_preview: Vector2 = Vector2()
     
     # Updates the space and time
-    def __call__(self):
+    def __call__(self, step: float) -> None:
 
         # Increases the amount of time experienced
-        self.time += self.step
+        self.time += step
 
         # Updates postion and velocity
-        self.velocity += self.acceleration * self.step
-        self.position += self.velocity * self.step
+        self.velocity += self.acceleration * step
+        self.position += self.velocity * step
 
         # Resets acceleration
         self.acceleration_preview: Vector2 = self.acceleration
@@ -36,11 +35,11 @@ class Spacetime:
 # A spacetime that incorporates special relativity.
 # Same as spacetime, but from an observer on the ground
 class RelativisticSpacetime(Spacetime):
-    def __init__(self, step, position: Vector2 = Vector2(), velocity: Vector2 = Vector2(), acceleration: Vector2 = Vector2()) -> None:
-        super().__init__(step, position, velocity, acceleration)
+    def __init__(self, position: Vector2 = Vector2(), velocity: Vector2 = Vector2(), acceleration: Vector2 = Vector2()) -> None:
+        super().__init__(position, velocity, acceleration)
 
-    def __call__(self) -> None:
         gamma = self.gamma()
+    def __call__(self, step: float) -> None:
 
         # Increases time experiences
         self.time += self.step / gamma
