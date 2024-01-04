0106f16612d1d04da93640fea78ebc35ccb710e0
Finkch <skyler@thealdersons.org>
2024-01-03 17:03:11 -0800

    Moved data flattening to Store

--- plot.py
+++ plot.py
@@ -1,21 +1,14 @@
 # Plots data
 
-import json
 import matplotlib.pyplot as mpl
 
 # Responsible for plotting data from text files
 class Plotter:
-    def __init__(self, file: str) -> None:
-        
-        # The file in which data is stored
-        self.file: str = file
+    def __init__(self, data: dict, metadata: dict) -> None:
 
-        self.metadata: dict = {}
-        self.data: dict = {}
-        self.unflattened: list = []
+        self.metadata: dict = metadata
+        self.data: dict = data
 
-        # Reads the data to memory
-        self.read()
 
 
     # Calling Plotter plots data
@@ -41,56 +34,3 @@ class Plotter:
     # Shows plot
     def show(self):
         mpl.show()
-
-
-    # Loads the text file to memory
-    def read(self) -> None:
-        with open(self.file, 'r') as file:
-            for line in file:
-                line = line.replace("'", '"') # Replaces single-quotes with double-quotes
-                self.unflattened.append(json.loads(line))
-
-        # Grabs the metadata from data
-        self.metadata = self.unflattened.pop(0)
-
-        # Flattens the dictionary to minimal depth
-        self.flatten()
-
-    # Flattens the data array so that it has minimal depth
-    def flatten(self):
-
-        # Iterates over every line
-        for line in self.unflattened:
-            self.recursive_flatten(line, '')
-
-        # Deletes unnecessary data
-        self.unflattened = None
-
-    # Recurse through the dictionary, adding items
-    def recursive_flatten(self, data: dict, path: str):
-        
-        # Iterate over all items at this depth
-        for key in data.keys():
-
-            # If the item is a dictionary, recurse
-            if isinstance(data[key], dict):
-                self.recursive_flatten(data[key], f'{path}{key}-')
-
-            # Otherwise, add the item to data
-            else:
-                self.add_flat(f'{path}{key}-', data[key])
-
-    # Adds the piece of data to the flattened list
-    def add_flat(self, key: str, item) -> None:
-
-        # Removes the trailing hyphen
-        key = key[:-1]
-
-        # If this is a new entry, make a list for it
-        if not key in self.data:
-            self.data[key] = []
-
-        # Add the item to data
-        self.data[key].append(item)
-
-
--- store.py
+++ store.py
@@ -1,7 +1,7 @@
 # Stores ALL information from a given simulation
 
 import sys
-
+import json
 
 # A Store stores data
 class Store:
@@ -13,6 +13,11 @@ class Store:
         
         # The file to which to write
         self.file: str = 'temp.txt'
+
+        # Used for reading data
+        self.metadata: dict = {}
+        self.data: dict = {}
+        self.unflattened: list = []
 
         # If supplied with some starting data, write it out immediatly
         if initial_data:
@@ -46,3 +51,61 @@ class Store:
     # Returns how large the data size is
     def data_size(self) -> int:
         return sys.getsizeof(self.data)
+    
+
+
+    # Reads data from a file, returning it as a dictionary
+    def read(self) -> dict:
+        with open(self.file, 'r') as file:
+            for line in file:
+                line = line.replace("'", '"') # Replaces single-quotes with double-quotes
+                self.unflattened.append(json.loads(line))
+
+        # Grabs the metadata from data
+        self.metadata = self.unflattened.pop(0)
+
+        # Flattens the dictionary to minimal depth
+        self.flatten()
+
+        return self.data, self.metadata
+
+    # Flattens the data array so that it has minimal depth
+    def flatten(self):
+
+        # Iterates over every line
+        for line in self.unflattened:
+            self.recursive_flatten(line, '')
+
+        # Deletes unnecessary data
+        self.unflattened = None
+
+    # Recurse through the dictionary, adding items
+    def recursive_flatten(self, data: dict, path: str):
+        
+        # Iterate over all items at this depth
+        for key in data.keys():
+
+            # If the item is a dictionary, recurse
+            if isinstance(data[key], dict):
+                self.recursive_flatten(data[key], f'{path}{key}-')
+
+            # Otherwise, add the item to data
+            else:
+                self.add_flat(f'{path}{key}-', data[key])
+
+    # Adds the piece of data to the flattened list
+    def add_flat(self, key: str, item) -> None:
+
+        # Removes the trailing hyphen
+        key = key[:-1]
+
+        # If this is a new entry, make a list for it
+        if not key in self.data:
+            self.data[key] = []
+
+        # Add the item to data
+        self.data[key].append(item)
+
+
+
+
