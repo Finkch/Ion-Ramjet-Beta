86cdd298eabb981fbd7a595abab37483ed95d573
Finkch <skyler@thealdersons.org>
2024-01-06 17:54:12 -0800

    Added comments

--- main_sim.py
+++ main_sim.py
@@ -5,10 +5,14 @@ from simulation import *
 # Gets everything going
 def main():
     
+    # The Ramjet to use in this simulation
     ramjet = 'ioRam-Beta'
 
+    # Whether to run in debug mode
+    # In debug mode, sim-steps occur in lock-step with printouts
     debug = False
 
+    # How many seconds are simulated in each step
     rate = 1
 
     # Desired framerate for printouts
@@ -17,8 +21,10 @@ def main():
     # The file to store data in
     file = 'temp.txt'
 
+    # Creates the simulation
     simulation = Simulation(rate, framerate, ramjet, file) if not debug else DebugSimulation(rate, framerate, ramjet, file) 
 
+    # Runs the simulation
     simulation()
 
 # Ready, set, go!
