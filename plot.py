75fa92bdedf1940c504b5b84e99b757795b8aceb
Finkch <skyler@thealdersons.org>
2024-01-09 10:41:53 -0800

    Added a function to Plotter to return the data, sliced

--- plot.py
+++ plot.py
@@ -15,8 +15,7 @@ class Plotter:
     def __call__(self, x_key: str, y_key: str, plot_name: str, axes_names: list[str, str], *args, **kwargs) -> None:
 
         # Grabs the necessary data
-        x = self.data[x_key]
-        y = self.data[y_key]
+        x, y = self.get_data(x_key, y_key, kwargs)
 
         # Grabs the figure and axes
         figure, axes = mpl.subplots()
@@ -43,3 +42,27 @@ class Plotter:
     # Shows plot
     def show(self):
         mpl.show()
+
+    # Obtains the data
+    def get_data(self, x_key: str, y_key: str, kwargs) -> tuple:
+
+        # The limits of the slice
+        x_start = 0
+        x_end = None
+
+        # Gets the first item closest to the asked starting value
+        if 'x_start' in kwargs:
+            for i in range(len(self.data[x_key])):
+                if self.data[x_key][i] >= kwargs['x_start']:
+                    x_start = i
+                    break
+        
+        # Gets the first item closest to the asked ending value
+        if 'x_end' in kwargs:
+            for i in range(0, len(self.data[x_key], -1)):
+                if self.data[x_key][i] <= kwargs['x_end']:
+                    x_end = i
+                    break
+
+        # Returns the data
+        return self.data[x_key][x_start:x_end], self.data[y_key][x_start:x_end]
